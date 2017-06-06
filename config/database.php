7fd60abb7f6a9a0bf921db7d7f4abb37f89f31af
iamchiriru <chithewebdeveloper@gmail.com>
2017-06-07 02:14:05 +0800

    From Local

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'heycommunity'),
+            'database'  => env('DB_DATABASE', 'ayoov3'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', 'sm1l1ngs4thew3Ak'),
             'port'      => env('DB_PORT', '3306'),
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
